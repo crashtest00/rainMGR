7589f0b4c8d787756e863d376627998d69e3ef1a
Jack <jjmarck@gmail.com>
2023-06-21 20:11:24 -0500

    Added some primary logic

--- Rainmaker/src/main.cpp
+++ Rainmaker/src/main.cpp
@@ -1,15 +1,25 @@
 #include <Arduino.h>
 
+RTC_DATA_ATTR int rtcPersist = 0; // Prototype for storing information in persistant RTC mem
+
 // put function declarations here:
 int myFunction(int, int);
 
+// Setup runs on every wake!!
 void setup() {
+  // Set target rainfall amount (server will handle the logic and pass a number)
+  // Create irrEvent object Object needs to return an array of irrigation OR have runZone function that handles 
+  // pinmode based on int passed to function. Protoype: runZone(zone=1,gpio=1) 
+
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
+  // What time does irrigation start? // TODO: Logic to handle multiple irrigation events each day
+  // After irrigation runs, calculate next irrigation time (Maybe the server delivers this?)
+  // Sleep until nextWake
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  // Do we even need a loop? What goes here?  
 }
 
 // put function definitions here:

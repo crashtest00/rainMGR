365061b6e50865a26eb435185c68601d924e9555
crashtest <jjmarck@gmail.com>
2023-06-22 20:38:31 -0500

    Resolve conflicts

--- Rainmaker/src/main.cpp
+++ Rainmaker/src/main.cpp
@@ -1,26 +1,12 @@
 #include <Arduino.h>
-<<<<<<< HEAD
-#include "zones.h"
-=======
 
 RTC_DATA_ATTR int rtcPersist = 0; // Prototype for storing information in persistant RTC mem
 
 // put function declarations here:
 int myFunction(int, int);
->>>>>>> origin/main
 
 // Setup runs on every wake!!
 void setup() {
-  // Set target rainfall amount (server will handle the logic and pass a number)
-  // Create irrEvent object Object needs to return an array of irrigation OR have runZone function that handles 
-  // pinmode based on int passed to function. Protoype: runZone(zone=1,gpio=1) 
-
-  // Target irrigation amount per week
-  int target = 50;
-
-  // Number of irrigation events per week
-  int freq = 7;
-=======
   // Set target rainfall amount (server will handle the logic and pass a number)
   // Create irrEvent object Object needs to return an array of irrigation OR have runZone function that handles 
   // pinmode based on int passed to function. Protoype: runZone(zone=1,gpio=1) 
